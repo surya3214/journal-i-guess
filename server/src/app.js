a97482a5652d8a96c4d00e2d972f4efe3ae7204d
Surya <surya3214@gmail.com>
2017-12-06 00:51:26 +0530

    express js server setup

--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,17 @@
-console.log('from app.js')
+const express = require('express')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const cors = require('cors')
+
+const app = express()
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.get('/status', (req, res) => {
+  res.send({
+    message: 'Hello world! Server is running'
+  })
+})
+
+app.listen(process.env.PORT || 8081)
